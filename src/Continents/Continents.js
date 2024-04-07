8b95835bc57fb33352883217a34bc34d508a973b
bonkeGcobo <bonke.gcobo@umdoko.com>
2024-04-07 22:55:21 +0200

    add african continent data

---
+++ src/Continents/Continents.js
@@ -0,0 +1,166 @@
+const africanCountries = [
+	{
+		name: 'Algeria',
+	},
+	{
+		name: 'Angola',
+	},
+	{
+		name: 'Benin',
+	},
+	{
+		name: 'Botswana',
+	},
+	{
+		name: 'Burkina Faso',
+	},
+	{
+		name: 'Burundi',
+	},
+	{
+		name: 'Cabo Verde',
+	},
+	{
+		name: 'Cameroon',
+	},
+	{
+		name: 'Central African Republic',
+	},
+	{
+		name: 'Chad',
+	},
+	{
+		name: 'Comoros',
+	},
+	{
+		name: 'Democratic Republic of the Congo',
+	},
+	{
+		name: 'Republic of the Congo',
+	},
+	{
+		name: 'Djibouti',
+	},
+	{
+		name: 'Egypt',
+	},
+	{
+		name: 'Equatorial Guinea',
+	},
+	{
+		name: 'Eritrea',
+	},
+	{
+		name: 'Eswatini',
+	},
+	{
+		name: 'Ethiopia',
+	},
+	{
+		name: 'Gabon',
+	},
+	{
+		name: 'Gambia',
+	},
+	{
+		name: 'Ghana',
+	},
+	{
+		name: 'Guinea',
+	},
+	{
+		name: 'Guinea-Bissau',
+	},
+	{
+		name: 'Ivory Coast',
+	},
+	{
+		name: 'Kenya',
+	},
+	{
+		name: 'Lesotho',
+	},
+	{
+		name: 'Liberia',
+	},
+	{
+		name: 'Libya',
+	},
+	{
+		name: 'Madagascar',
+	},
+	{
+		name: 'Malawi',
+	},
+	{
+		name: 'Mali',
+	},
+	{
+		name: 'Mauritania',
+	},
+	{
+		name: 'Mauritius',
+	},
+	{
+		name: 'Morocco',
+	},
+	{
+		name: 'Mozambique',
+	},
+	{
+		name: 'Namibia',
+	},
+	{
+		name: 'Niger',
+	},
+	{
+		name: 'Nigeria',
+	},
+	{
+		name: 'Rwanda',
+	},
+	{
+		name: 'Sao Tome and Principe',
+	},
+	{
+		name: 'Senegal',
+	},
+	{
+		name: 'Seychelles',
+	},
+	{
+		name: 'Sierra Leone',
+	},
+	{
+		name: 'Somalia',
+	},
+	{
+		name: 'South Africa',
+	},
+	{
+		name: 'South Sudan',
+	},
+	{
+		name: 'Sudan',
+	},
+	{
+		name: 'Tanzania',
+	},
+	{
+		name: 'Togo',
+	},
+	{
+		name: 'Tunisia',
+	},
+	{
+		name: 'Uganda',
+	},
+	{
+		name: 'Zambia',
+	},
+	{
+		name: 'Zimbabwe',
+	},
+];
+
+export default africanCountries;
